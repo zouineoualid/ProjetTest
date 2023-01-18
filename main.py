ef9f637a45c4baa48f79582e74fb1ce2e3f370f6
Mynato7 <qklopocki@hotmail.fr>
2023-01-18 12:16:20 +0100

    Lol

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ a=1
 # Condition 
 if false : # Changement ici
     print "c'est vrai"
+    print "c'est faux"
     break
 elif a==2
     print " Deux"
     break
 else :
     print("autre")
+
+while i<10 :
+    print "Bonjour +i "
 
