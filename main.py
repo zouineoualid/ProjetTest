da15ae973b42178d2b2a78f5f2ace8a0daa627c5
zouineoualid <zouineoualid@gmail.com>
2023-01-18 12:23:39 +0100

    modification de main.py afin d'ajouter des acces à 3commas

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 print ("Bonjour mon équipe ")
 # Déclaration de la variable
 a=1
@@ -14,6 +17,30 @@ elif a==2
 else :
     print("autre")
 
+<<<<<<< HEAD
 while i<10 :
     print "Bonjour +i "
 
+=======
+#les clés du token pour accéder
+key='72fa3df10a62414dssfsfsa1a193876b9eefcbab2f8656c114215dfsdfcd4c4183f35796c7c279a5'
+secret='afa5d8fbb8030b922f80dsffds2151e159f027cd642a939be23d1e823a549618e3f8d484ca53b777bdb196cb6b39b3a9d96fc9d96701826aeecff93e947cc2bb9fa88b218f7994f699689b53ab8445s1dfs51sdfdea95cb4eb33bb145214dsfsfsdygsdhv142dfs12zZ24d5sfsdf1'
+# Définir l'URL de l'API
+api_url = 'https://3commas.io/api/v3/bots'
+
+# Définir le jeton d'accès et la clé privée
+access_token = key
+private_key = secret
+
+# Préparer la demande
+headers = {
+    'Authorization': 'Bearer ' + str(access_token),
+    'X-Api-Key': private_key
+}
+
+# Envoyer la demande
+response = requests.get(api_url, headers=headers)
+
+# Afficher la réponse
+print(response.json())
+>>>>>>> d2ec0eb (modification de main.py afin d'ajouter des acces à 3commas)
